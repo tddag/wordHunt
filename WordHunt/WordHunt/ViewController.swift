fc731de9535548dcb6a502ed2991489655d5ff07
Chris Santin <43220516+csantin@users.noreply.github.com>
2019-03-15 16:13:21 -0400

    Finished UI Prototype

--- WordHunt/WordHunt/ViewController.swift
+++ WordHunt/WordHunt/ViewController.swift
@@ -8,13 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var users: [String] = ["Hao", "Tam", "Quan", "Chris", "Player", "Test"]
+    var scores: [String] = ["4:00", "4:12", "4:56", "5:15", "7:27", "10:00"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (users.count);
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = users[indexPath.row] + "   -   " + scores[indexPath.row]
+        return (cell)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
